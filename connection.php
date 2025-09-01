e3744300e66fc5aebe570818176ea7eedaf206eb
sony-sujal <sonysujal77@gmail.com>
2025-09-01 21:17:35 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,10 +7,10 @@ $password = "Sujal8055"; // Your InfinityFree MySQL password
 $dbname = "if0_39833008_edoc";   // Your InfinityFree MySQL database name
 
 // Create database connection
-$conn = mysqli_connect($host, $user, $password, $dbname);
+$database = mysqli_connect($host, $user, $password, $dbname);
 
 // Check connection
-if (!$conn) {
+if (!$database) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
